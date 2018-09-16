af6c16f30bbe2de01b052580eb0970adecc255e9
Yoonsik Jung <developer.kelvin@gmail.com>
2018-09-16 16:30:25 +0900

    Update README.md Update dcinside_crawler code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Crawling Seminar
+Crawling Seminar
+=============
+
+20180919 Crawling Seminar\
+Jung Yoon Sik, Human Factors and Ergonomics Lab., Dongguk Univ.
+
+## Description
+크롤링 세미나에서 사용했던 발표자료와 예제 코드입니다.
+실습 환경은 Anaconda Python 3.6입니다.
+
+## Setting
+<pre><code>
+pip install -r requirements.txt
+</code></pre>
+
+
+## Run
+<pre><code>
+python naver_crawler.py
+python dcinside_crawler.py
+</code></pre>
--- dcinside_crawler.py
+++ dcinside_crawler.py
@@ -4,45 +4,53 @@
 from bs4 import BeautifulSoup
 import requests
 
-article_url = 'http://gall.dcinside.com/board/view/'
+
 comment_url = 'http://gall.dcinside.com/comment/view'
 
-session = requests.Session()
 
-headers = {
-    'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-}
+# 식물 갤러리의 게시글을 가져온다.
+def get_article(article_no):
+    article_url = 'http://gall.dcinside.com/board/view/'
 
-gall_name = 'tree'
+    session = requests.Session()
 
-article_no = 243965
+    headers = {
+        'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+    }
 
-params = {'id': gall_name, 'no': article_no}
+    gall_name = 'tree'
 
-response = session.get(article_url, headers=headers, params=params)
+    params = {'id': gall_name, 'no': article_no}
 
-soup = BeautifulSoup(response.text, 'html.parser')
+    response = session.get(article_url, headers=headers, params=params)
 
-a_user_info = soup.find(attrs={'class': 'user_layer'})
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-article_title = soup.select('.wt_subject > dd')[0].get_text().strip()
+    a_user_info = soup.find(attrs={'class': 'user_layer'})
 
-user_id = a_user_info['user_id'].strip()
+    article_title = soup.select('.wt_subject > dd')[0].get_text().strip()
 
-nickname = a_user_info['user_name'].strip()
+    user_id = a_user_info['user_id'].strip()
 
-ip_address = soup.find(attrs={'class': 'li_ip'}).get_text().strip()
+    nickname = a_user_info['user_name'].strip()
 
-view_cnt = soup.find_all(attrs={'class': 'dd_num'})[0].get_text().strip()
+    ip_address = soup.find(attrs={'class': 'li_ip'}).get_text().strip()
 
-comment_cnt = soup.find_all(attrs={'class': 'dd_num'})[1].get_text().strip()
+    view_cnt = soup.find_all(attrs={'class': 'dd_num'})[0].get_text().strip()
 
-content = soup.table.td.get_text().strip()
+    comment_cnt = soup.find_all(attrs={'class': 'dd_num'})[1].get_text().strip()
 
-reg_dtime = soup.find('b').get_text().strip()
+    content = soup.table.td.get_text().strip()
 
-article_data = (gall_name, article_no, article_title, user_id, nickname,
-                ip_address, view_cnt, comment_cnt, content, reg_dtime)
+    reg_dtime = soup.find('b').get_text().strip()
 
-print(article_data)
+    article_data = (gall_name, article_no, article_title, user_id, nickname,
+                    ip_address, view_cnt, comment_cnt, content, reg_dtime)
+
+    print(article_data)
+
+
+if __name__ == "__main__":
+    get_article(243965)
+
